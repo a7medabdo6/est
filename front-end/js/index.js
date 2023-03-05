dfbd17bdc5f03b01b0bacae73b6ad38c82494637
a7medabdo6 <57114105+a7medabdo6@users.noreply.github.com>
2023-03-05 23:28:57 +0200

    Update index.js

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -30,7 +30,7 @@ const loginBtn = document.getElementById("submitBtn");
 
 loginBtn.addEventListener("click", () => {
     const data = { email: email.value, password: password.value };
-    fetch("http://65.108.33.225:3000/login", {
+    fetch("http://165.227.163.66/api/login", {
       method: "POST",
       headers:{'Content-Type':'application/json'},
       body:JSON.stringify(data)
